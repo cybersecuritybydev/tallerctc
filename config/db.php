2500072f0cad4eed27728c76dd2da71e7435ae06
Peter Fernández <peter@appsweb.dev>
2022-08-09 20:44:13 -0300

    agrego inicio de sesión inseguro

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost'); //DB_HOST
+define('DB_USER', 'root'); //Usuario de tu base de datos
+define('DB_PASS', ''); //Contraseña del usuario de la base de datos
+define('DB_NAME', 'login_taller');//Nombre de la base de datos
