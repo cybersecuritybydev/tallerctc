4462561d7ac635726f9a6a472d3c8a5c34a4d0a3
Peter Fernández <peter@appsweb.dev>
2022-08-09 23:01:37 -0300

    agrego protección al recolector de cookies

--- recolector_de_cookies.php
+++ recolector_de_cookies.php
@@ -1,4 +1,7 @@
 <?php
 if (isset($_GET["cookie"])) {
-    echo $_GET["cookie"];
+    //Esto nos ocasionaría problemas: echo $_GET["cookie"];
+    //Aquí tengo una función que PHP 8 ya no utiliza:
+    //echo filter_input(INPUT_GET, 'cookie', FILTER_SANITIZE_STRING);
+    echo htmlspecialchars($_GET["cookie"]);
 }
